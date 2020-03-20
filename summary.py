71b07f2ff80e18ba90a862ae47bc532311f820ca
y-chan <y_chan_crypto@yahoo.co.jp>
2020-03-20 14:42:47 +0900

    refactor and add sickbeds_summary

--- summary.py
+++ summary.py
@@ -6,8 +6,29 @@ from util import get_file, SUMMARY_INIT, get_numbers_in_text
 
 class MainSummary:
     def __init__(self):
-        self.summary = SUMMARY_INIT
+        self.pdf_texts = get_file('/kk03/corona_hasseijyokyo.html', "pdf")
+        self.sickbeds_count = 154
         self.values = []
+        self._main_summary = {}
+        self._sickbeds_summary_json = {}
+
+    def main_summary_json(self) -> Dict:
+        if not self._main_summary:
+            self.make_main_summary()
+        return self._main_summary
+
+    def sickbeds_summary_json(self) -> Dict:
+        if not self._sickbeds_summary_json:
+            self.make_sickbeds_summary()
+        return self._sickbeds_summary_json
+
+    def make_main_summary(self) -> None:
+        self._main_summary = SUMMARY_INIT
+        self._main_summary['last_update'] = self.get_last_update()
+
+        content = ''.join(self.pdf_texts[3:])
+        self.values = get_numbers_in_text(content)
+        self.set_summary_values(self._main_summary)
 
     def set_summary_values(self, obj) -> None:
         obj['value'] = self.values[0]
@@ -16,18 +37,19 @@ class MainSummary:
                 self.values = self.values[1:]
                 self.set_summary_values(child)
 
-    def get_summary_json(self) -> Dict:
-        pdf_texts = get_file('/kk03/corona_hasseijyokyo.html', "pdf")
-
-        # Set summary values
-        content = ''.join(pdf_texts[3:])
+    def make_sickbeds_summary(self) -> None:
+        content = ''.join(self.pdf_texts[3:])
         self.values = get_numbers_in_text(content)
-        self.set_summary_values(self.summary)
+        self._sickbeds_summary_json = {
+            "data": {
+                "入院患者数": self.values[2],
+                "残り病床数": self.sickbeds_count - self.values[2]
+            },
+            "last_update": self.get_last_update()
+        }
 
-        # Set last update
-        caption = pdf_texts[0]
+    def get_last_update(self) -> str:
+        caption = self.pdf_texts[0]
         dt_vals = get_numbers_in_text(caption)
         last_update = datetime(datetime.now().year, dt_vals[0], dt_vals[1]) + timedelta(hours=dt_vals[2])
-        self.summary['last_update'] = datetime.strftime(last_update, '%Y-%m-%dT%H:%M:%S+09:00')
-
-        return self.summary
+        return datetime.strftime(last_update, '%Y/%m/%d %H:%M')
